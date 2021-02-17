fb2f476f29db49f39af008bb589e6d7d5671daaf
robertGGA <gadelshinrob@gmail.com>
2021-02-17 23:04:22 +0300

    first task completed

--- js/firstfile.js
+++ js/firstfile.js
@@ -1,5 +1,19 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {};
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    private : false
+};
+
+const watchedMovie = prompt("Один из последних просмотренных вами фильмов?", ""),
+    rateMovie = prompt("Ваша оценка?", "");
+
+personalMovieDB.movies[watchedMovie] = rateMovie;
+
+console.log(personalMovieDB);
+
